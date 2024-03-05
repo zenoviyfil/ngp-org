7ef809f8ee3f7bb3cd9fd58915917ea85dc7b263
Zenoviy Fil <98259328+zenoviyfil@users.noreply.github.com>
2024-03-05 13:18:42 +0100

    new commit

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -5,37 +5,31 @@ import { SocialIcon } from 'react-social-icons'
 const Navbar = () => {
   return (
     <div className={css.headerNavbar}>
-      <a href="#index.html">
+      {/* <div className={css.logoDiv}>
         <img className={css.logo} src="" alt="logo" />
-        <p className={css.npg}>NEW POWERLIFTING GENERATION</p>
-      </a>
+        <a className={css.npgLink} href="#index.html">
+          <p>NEW POWERLIFTING GENERATION</p>
+        </a>
+      </div> */}
       <div className={css.linksBar}>
         <ul className={css.navMenu}>
-          <li className={css.navMenuBtn}>Головна</li>
-          <li className={css.navMenuBtn}>Блог</li>
-          <li className={css.navMenuBtn}>Календар</li>
-          <li className={css.navMenuBtn}>Про нас</li>
+          <li><a className={css.navMenuBtn} href='#index.html'>Головна</a></li>
+          <li><a className={css.navMenuBtn} href='#blog.html'>Блог</a></li>
+          <li><a className={css.navMenuBtn} href='#calendar.html'>Календар</a></li>
+          <li><a className={css.navMenuBtn} href='#about.html'>Про нас</a></li>
         </ul>
         <ul className={css.socials}>
-          <li className={css.socialLink}>
-            <a href="" target="_blank" title="Facebook">
-            <SocialIcon bgColor='none' fgColor='black' url='https://facebook.com' />
-            </a>
+          <li>
+            <SocialIcon  className={css.socialLink} bgColor='none' fgColor='black' url='https://facebook.com' />
           </li>
-          <li className={css.socialLink}>
-            <a href="" target="_blank" title="Facebook">
-            <SocialIcon bgColor='none' fgColor='black' url='https://twitter.com' />
-            </a>
+          <li>
+            <SocialIcon  className={css.socialLink} bgColor='none' fgColor='black' url='https://twitter.com' />
           </li>
-          <li className={css.socialLink}>
-            <a href="" target="_blank" title="Facebook">
-            <SocialIcon bgColor='none' fgColor='black' url='https://instagram.com' />
-            </a>
+          <li>
+            <SocialIcon  className={css.socialLink} bgColor='none' fgColor='black' url='https://instagram.com' />
           </li>
-          <li className={css.socialLink}>
-            <a href="" target="_blank" title="Facebook">
-            <SocialIcon bgColor='none' fgColor='black' url='https://youtube.com' />
-            </a>
+          <li>
+            <SocialIcon  className={css.socialLink} bgColor='none' fgColor='black' url='https://youtube.com' />
           </li>
         </ul>
       </div>
